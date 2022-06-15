b211b962c9553646f30e91a2947658cf6f37842b
MustafaErenn <18360859024@btu.ogrenci.edu.tr>
2022-06-16 00:58:45 +0300

    rehber sayfasina arama ozelligi eklendi

--- lib/features/contacts/contacts_view_model.dart
+++ lib/features/contacts/contacts_view_model.dart
@@ -4,6 +4,10 @@ import 'package:accompany/services/contact_service.dart';
 import 'package:flutter/material.dart';
 
 abstract class ContactsViewModel extends State<ContactsView> {
+  List<ContactModel> matchedContacts = [];
+
+  bool isSearched = false;
+
   bool _isLoading = false;
   bool get isLoading {
     return _isLoading;
@@ -13,6 +17,30 @@ abstract class ContactsViewModel extends State<ContactsView> {
     setState(() {
       _isLoading = !_isLoading;
     });
+  }
+
+  void checkList(String query) {
+    setState(() {
+      matchedContacts.clear();
+    });
+    if (query != "") {
+      setState(() {
+        isSearched = true;
+      });
+      for (var contact in contactList!) {
+        if ((contact.name!.toLowerCase() + contact.surname!.toLowerCase())
+            .contains(query.toLowerCase())) {
+          setState(() {
+            matchedContacts.add(contact);
+          });
+        }
+      }
+    } else {
+      setState(() {
+        matchedContacts.clear();
+        isSearched = false;
+      });
+    }
   }
 
   List<ContactModel>? contactList;
